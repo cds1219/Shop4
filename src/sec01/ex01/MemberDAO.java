fc64a24e6a4a102abd2d221eaac3ab8be1093d69
cds1219 <cds1219_@naver.com>
2021-06-26 11:21:24 +0900

    회원정보조회

---
+++ src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,75 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private DataSource dataFactory;
+	private Connection conn;
+	private PreparedStatement pstmt;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/MyDB");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<MemberVO> listMembers() {
+		List<MemberVO> membersList = new ArrayList();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from  users";	//SQL문
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);	//ps객체 생성 & SQL문 전달
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				String name = rs.getString("name");
+				String address = rs.getString("address");
+				MemberVO memberVO = new MemberVO(id, pw, name, address);	//조회한 회원정보를 레코드별로 MemberVO객체의 속성에 저장
+				membersList.add(memberVO);	//memberList에 MemberVO객체들을 차례로 저장
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return membersList;
+	}
+
+	public void addMember(MemberVO m) {
+		try {
+			conn = dataFactory.getConnection();
+			String id = m.getId();
+			String pw = m.getPw();
+			String name = m.getName();
+			String address = m.getAddress();
+			String query = "INSERT INTO t_member(id, pw, name, address)" + " VALUES(?, ? ,? ,?)";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);	//ps객체 생성 & SQL문 전달
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, name);
+			pstmt.setString(4, address);
+			pstmt.executeUpdate();					//SQL문 실행
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
